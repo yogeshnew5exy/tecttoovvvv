59a9a9559d700676b58e23f1a2fb70d20b6cc60f
VIDEO-COMPRESSOR <122086028+VIDEO-COMPRESSOR@users.noreply.github.com>
2023-03-19 21:46:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,47 @@ auth_users = [5748425435,5579690619, 5775042099,1327415906]
 sudo_users = auth_users
 sudo_groups = [-1001717517834]
 
+shell_usage = f"**USAGE:** Executes terminal commands directly via bot.\n\n<pre>/shell pip install requests</pre>"
+def one(user_id):
+    if user_id in sudo_users:
+        return True
+    return False
+@bot.on_message(filters.command(["shell"]))
+async def shell(client, message: Message):
+    """
+    Executes terminal commands via bot.
+    """
+    logging.info('hh')
+    if not one(message.from_user.id):
+        return
+
+    if len(message.command) < 2:
+        return await message.reply_text(shell_usage, quote=True)
+
+    user_input = message.text.split(None, 1)[1].split(" ")
+
+    try:
+        shell = subprocess.Popen(
+            user_input, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+
+        stdout, stderr = shell.communicate()
+        result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+
+    except Exception as error:
+        logging.info(f"{error}")
+        return await message.reply_text(f"**Error**:\n\n{error}", quote=True)
+
+    if len(result) > 2000:
+        file = BytesIO(result.encode())
+        file.name = "output.txt"
+        await message.reply_text("Output is too large (Sending it as File)", quote=True)
+        await client.send_document(message.chat.id, file, caption=file.name)
+    else:
+        await message.reply_text(f"**Output:**:\n\n{result}", quote=True)
+
+
+
 keyboard = InlineKeyboardMarkup(
     [
         [
