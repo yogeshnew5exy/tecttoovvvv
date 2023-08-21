b33763df069c33b2a9fa7e0d26323021ea73592e
tiger7815 <141488144+tiger7815@users.noreply.github.com>
2023-08-21 06:17:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [5908818236,5942085615]
+auth_users = [5908818236,5587431966]
 sudo_users = auth_users
 sudo_groups = [-1001911553062]
 
